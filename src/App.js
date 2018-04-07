456c487d2e53920152c67503053317367c9a07e6
yshr446 <yshr446@gmail.com>
2018-04-07 23:59:12 +0900

    Integrate with tinyMCE

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    handleEditorChange = (e) => {
+        console.log('Content was updated:', e.target.getContent());
+    }
+
+    render() {
+        return (
+            <Editor
+                initialValue="<p>This is the initial content of the editor</p>"
+                init={{
+                    plugins: 'link image code',
+                    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+                }}
+                onChange={this.handleEditorChange}
+            />
+        );
+    }
 }
 
 export default App;
